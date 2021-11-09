a2818c3e96db6cb906b401bb6b14b32053d691c5
metaGmetapop <45126925+metaGmetapop@users.noreply.github.com>
2021-11-09 13:54:15 -0500

    Add files via upload

--- metapop/__init__.py
+++ metapop/__init__.py
@@ -2,3 +2,5 @@ from .metapop_main import *
 from .metapop_helper_functions import *
 from .metapop_filter import *
 from .metapop_snp_call import *
+from .metapop_mine_reads import *
+from .metapop_fst import *
