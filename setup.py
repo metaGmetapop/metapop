0136f1f99993d9f9723eb0afa71eb69a24c893d8
metaGmetapop <45126925+metaGmetapop@users.noreply.github.com>
2022-03-07 17:13:11 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="metapop",
+    version="0.0.42",
+    author="Kenji Gerhardt",
+    author_email="kenji.gerhardt@gmail.com",
+    description="A metagenomic sta		tistic pipeline.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+	py_modules=["metapop"],
+	include_package_data=True,
+    python_requires='>=3',
+	entry_points={
+        "console_scripts": [
+            "metapop=metapop.metapop_main:main",
+        ]
+    }
+)
